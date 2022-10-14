b8382bccee44068d5cde77f209742657a1e3822e
Naoki Tagami <n.tagami@kakehashi.life>
2022-10-14 09:34:24 +0900

    add trpc-client-devtools-link

--- apps/web/util/trpc.ts
+++ apps/web/util/trpc.ts
@@ -2,6 +2,7 @@
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "api";
 import { httpBatchLink } from "@trpc/client";
+import { devtoolsLink } from "trpc-client-devtools-link";
 
 function getBaseUrl() {
   return "http://localhost:4000";
@@ -11,6 +12,11 @@ export const trpc = createTRPCNext<AppRouter>({
   config({ ctx }) {
     return {
       links: [
+        devtoolsLink({
+          // `enabled` is true by default
+          // If you want to use the devtools extension just for development, do the following
+          //enabled: process.env.NODE_ENV === "development",
+        }),
         httpBatchLink({
           /**
            * If you want to use SSR, you need to use the server's full URL
